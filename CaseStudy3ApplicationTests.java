cb32ad704a79caebb1b508f38416fe868e912bca
ramya123553 <113048141+ramya123553@users.noreply.github.com>
2022-10-12 17:41:44 +0530

    Add files via upload

---
+++ CaseStudy3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gl.caseStudy3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseStudy3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
